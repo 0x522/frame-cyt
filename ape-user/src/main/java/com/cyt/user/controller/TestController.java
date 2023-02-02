e8f83236a778b27d7d5416f8b4417a9614d908b8
chenyuntao <chenyuntao@msunsoft.com>
2023-02-02 19:08:59 +0800

    Jackson实现对日期的格式化

--- ape-user/src/main/java/com/cyt/user/controller/TestController.java
+++ ape-user/src/main/java/com/cyt/user/controller/TestController.java
@@ -4,11 +4,10 @@ import com.cyt.redis.util.RedisShareLockUtil;
 import com.cyt.redis.util.RedisUtil;
 import com.cyt.tool.Base64Utils;
 import com.cyt.tool.PinYin4jUtils;
+import com.cyt.user.entity.po.UserPo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -56,6 +55,11 @@ public class TestController {
         } catch (Exception e) {
             log.error("异常");
         }
+    }
+
+    @PostMapping("/testQuery")
+    public void testQuery(@RequestBody UserPo user){
+        System.out.println(user);
     }
 
 }
